3b686b3a955ef68cadc02c974c225affd61eed72
RinZ26 <ivs1996@mail.ru>
2020-05-18 15:23:35 +0500

    1. Что такое итератор.[#291359]

---
+++ chapter_001_DataStructure_Algorithms/src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -0,0 +1,48 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс - итератор для массива. Возвращает элементы в обратном порядке.
+ *
+ * @author RinZ26
+ */
+
+public class BackwardArrayIt implements Iterator<Integer> {
+    /**
+     * Массив данных
+     */
+    private final int[] data;
+    /**
+     * Указатель на текущий обрабатываемый элемент
+     */
+    private int point;
+
+
+    public BackwardArrayIt(int[] data) {
+	this.data = data;
+	point = data.length - 1;
+    }
+
+    /**
+     * Проверка, есть ли следующий элемент
+     * @return ~
+     */
+    @Override
+    public boolean hasNext() {
+	return point >= 0;
+    }
+
+    /**
+     * Возвращаем элемент в обратном порядке с учетом проверки hasNext
+     * @return ? элемент : Exception
+     */
+    @Override
+    public Integer next() {
+	if (!hasNext()) {
+	    throw new NoSuchElementException();
+	}
+	return data[point--];
+    }
+}
