76c9fc85727de1e05c7164652bc8a82c59efd8ed
RinZ26 <ivs1996@mail.ru>
2020-05-18 18:39:55 +0500

    5.1.4. Создать convert(Iterator<Iterator>)[#291358]

---
+++ chapter_001_DataStructure_Algorithms/src/main/java/ru/job4j/it/Converter.java
@@ -0,0 +1,53 @@
+package ru.job4j.it;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс - для работы с итераторами
+ */
+public class Converter {
+    /**
+     * Возвращает итератор, перебирающий все элементы из различных итераторов
+     *
+     * @param it итератор итераторов
+     * @return ~
+     */
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+	return new Iterator<Integer>() {
+	    /**
+	     * Текущий итератор, что-то а-ля pointer в обычном итераторе
+	     */
+	    Iterator<Integer> currentIterator = Collections.emptyIterator();
+
+	    /**
+	     * Проверка, есть ли хоть где-нибудь элемент
+	     * @return ~
+	     */
+	    @Override
+	    public boolean hasNext() {
+		while (it.hasNext() || currentIterator.hasNext()) {
+		    if (currentIterator.hasNext()) {
+			return true;
+		    } else {
+			currentIterator = it.next();
+		    }
+		}
+		return false;
+	    }
+
+	    /**
+	     * Берем следущий элемент, если есть
+	     * @return ~
+	     */
+	    @Override
+	    public Integer next() {
+		if (!hasNext()) {
+		    throw new NoSuchElementException();
+		}
+		return currentIterator.next();
+	    }
+	};
+    }
+}
