4ac836961e435663ed5d0b1d4a0c28c1eed4abb0
RinZ26 <ivs1996@mail.ru>
2020-05-19 14:30:11 +0500

    5.2.1. Реализовать SimpleArray<T>[#291366]

---
+++ chapter_001_DataStructure_Algorithms/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,146 @@
+package ru.job4j.generic;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * Класс - обертка для массивов
+ *
+ * @param <T> тип массива
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    /**
+     * Хранилище данных
+     */
+    private Object[] data;
+    /**
+     * Указатель на следующую свободную ячейку;
+     */
+    private int pointer;
+
+    /**
+     * Основной конструктор, где мы создаем массив объектов размером @param size
+     *
+     * @param size размер массива
+     */
+    public SimpleArray(int size) {
+	this.data = new Object[size];
+    }
+
+    /**
+     * Добавление элемента
+     *
+     * @param model элемент
+     * @return можно ли добавить
+     */
+    public boolean add(T model) {
+	if (pointer < data.length) {
+	    data[pointer++] = model;
+	    return true;
+	}
+	return false;
+    }
+
+    /**
+     * Присвоение значения элементу
+     *
+     * @param index - индекс
+     * @param model - новое значение
+     * @return успех операции в зависимости от isIndexValid
+     */
+    public boolean set(int index, T model) {
+	boolean result = isIndexValid(index);
+	if (result) {
+	    data[index] = model;
+	}
+	return result;
+    }
+
+    /**
+     * Удаление элемента из массива путем смещения всех ячеек влево и заNULLение последнего элемента
+     * pointer также уменьшается
+     *
+     * @param index индекс удаляемого элемента
+     * @return успех операции в зависимости от isIndexValid
+     */
+    public boolean remove(int index) {
+	boolean result = isIndexValid(index);
+	if (result) {
+	    if (data.length - 1 - index >= 0) {
+		System.arraycopy(data, index + 1, data, index, data.length - 1 - index);
+	    }
+	    data[data.length - 1] = null;
+	    pointer--;
+	}
+	return result;
+    }
+
+    /**
+     * Получение элемента по индексу с учетом isIndexValid
+     *
+     * @param index - индекс
+     * @return элемент / null
+     */
+    public T get(int index) {
+	if (isIndexValid(index)) {
+	    return (T) data[index];
+	} else {
+	    return null;
+	}
+    }
+
+    /**
+     * Проверка, что индекс валидный
+     *
+     * @param index индекс
+     * @return true/false;
+     */
+    private boolean isIndexValid(int index) {
+	try {
+	    Objects.checkIndex(index, data.length);
+	} catch (IndexOutOfBoundsException indexOutOf) {
+	    System.out.println(indexOutOf.getClass());
+	    return false;
+	}
+	return true;
+    }
+
+    /**
+     * Возвращает размер массива date
+     *
+     * @return ~
+     */
+    public int size() {
+	return data.length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+	return new Iterator() {
+	    /**
+	     * Указатель внутри итератора на следующий элемент
+	     */
+	    int pointer;
+
+	    /**
+	     * Копируем содержимое иттерируемой коллекции
+	     */
+	    Object[] data = SimpleArray.this.data;
+
+	    @Override
+	    public boolean hasNext() {
+		return pointer < data.length;
+	    }
+
+	    @Override
+	    public T next() {
+		if (hasNext()) {
+		    return (T) data[pointer++];
+		} else {
+		    throw new NoSuchElementException();
+		}
+	    }
+	};
+    }
+}
---
+++ chapter_001_DataStructure_Algorithms/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.generic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+    SimpleArray<Integer> integerSimpleArray;
+    SimpleArray<String> stringSimpleArray;
+
+    @Before
+    public void setUp() {
+	integerSimpleArray = new SimpleArray<>(5);
+	stringSimpleArray = new SimpleArray<>(0);
+    }
+
+    @Test
+    public void add() {
+	integerSimpleArray.add(3);
+	assertThat(integerSimpleArray.get(0), is(3));
+    }
+
+    @Test
+    public void wrongIndexWhenAdd() {
+       assertFalse(stringSimpleArray.add("blabla"));
+    }
+    @Test
+    public void wrongIndexWithGet() {
+        assertFalse(stringSimpleArray.remove(stringSimpleArray.size() + 1));
+    }
+
+    @Test
+    public void set() {
+        integerSimpleArray.add(1);
+        integerSimpleArray.set(0, 2);
+        assertThat(integerSimpleArray.get(0), is(2));
+    }
+
+    @Test
+    public void remove() {
+        integerSimpleArray.add(5);
+        integerSimpleArray.remove(0);
+        assertNull(integerSimpleArray.get(0));
+    }
+
+    @Test
+    public void iterator() {
+        integerSimpleArray.add(3);
+        integerSimpleArray.add(2);
+        Iterator iterator = integerSimpleArray.iterator();
+        assertTrue(iterator.hasNext());
+        assertThat(iterator.next(), is(3));
+    }
+}
