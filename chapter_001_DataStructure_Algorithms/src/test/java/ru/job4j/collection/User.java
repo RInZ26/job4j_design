bbf3309ebb07dd5cc7860aeef0fe7da20a4047c9
RinZ26 <ivs1996@mail.ru>
2020-05-21 13:54:50 +0500

    1. Создать модель User[999#291373]

---
+++ chapter_001_DataStructure_Algorithms/src/test/java/ru/job4j/collection/User.java
@@ -0,0 +1,49 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+/**
+ * Класс - модель данных
+ */
+public class User {
+    private String name;
+    private int age;
+    private Calendar birthday;
+
+    /**
+     * Понтовый конструктор через сеттеры
+     * @param name ~
+     * @param age ~
+     * @param birthday ~
+      */
+    public User(String name, int age, Calendar birthday) {
+	setName(name);
+	setAge(age);
+	setBirthday(birthday);
+
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public int getAge() {
+	return age;
+    }
+
+    public void setAge(int age) {
+	this.age = age;
+    }
+
+    public Calendar getBirthday() {
+	return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+	this.birthday = birthday;
+    }
+}
