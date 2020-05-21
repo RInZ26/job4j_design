5f64a8b96039abe8c23305e744a02133d2518e8f
RinZ26 <ivs1996@mail.ru>
2020-05-21 13:37:24 +0500

    1. Реализовать коллекцию Set на массиве[996#291370]

---
+++ chapter_001_DataStructure_Algorithms/src/test/java/ru/job4j/collection/SimpleSetTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.collection;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleSetTest {
+    SimpleSet<String> stringSimpleSet;
+    String suno = "TL";
+    String rin = "prince";
+
+    @Before
+    public void setUp() {
+        stringSimpleSet = new SimpleSet<>();
+    }
+
+    @Test
+    public void addAndIter() {
+	stringSimpleSet.add(suno);
+	Iterator<String> tempIterator = stringSimpleSet.iterator();
+	assertThat(tempIterator.next(), is(suno));
+	assertFalse(tempIterator.hasNext());
+    }
+
+    @Test
+    public void addCopies() {
+        stringSimpleSet.add(suno);
+        stringSimpleSet.add(rin);
+        stringSimpleSet.add(suno);
+        Iterator<String> tempIterator = stringSimpleSet.iterator();
+        assertThat(tempIterator.next(), is(suno));
+        assertThat(tempIterator.next(), is(rin));
+        assertFalse(tempIterator.hasNext());
+    }
+}
