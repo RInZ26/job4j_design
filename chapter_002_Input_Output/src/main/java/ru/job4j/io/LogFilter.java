8604448753176c4b387257263754ce0fdc594fb0
RinZ26 <ivs1996@mail.ru>
2020-05-29 11:48:18 +0500

    0.3. BufferedReader.[252489#291398] Исправления глупой архитектуры

--- chapter_002_Input_Output/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002_Input_Output/src/main/java/ru/job4j/io/LogFilter.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -11,32 +12,18 @@ import java.util.stream.Collectors;
  */
 public class LogFilter {
     /**
-     * Элегантно через фильтер проходим по стриму, можно было сделать ещё лучше и добавить в параметры предикат и проверять по фильтру уже его
-     * А так алгоритм: Получаем заспличенный по /r лист строк из getListOfDataFromFile
-     * Через стрим фльитруем его по критерию, предварительно расспличивая каждую строку по пробелам
+     * Может показаться, что фильтер занимается лишним, но нет.
+     * Так как у нас паттерн, мы можем его использовать и для просто копирования из файла
+     * указав \\?* в pattern
      */
-    public static List<String> filter(String file) {
-	return getListOfDataFromFile(file).stream().filter(o -> {
-	    String[] splittedO = o.split(" ");
-	    return Integer.parseInt(splittedO[splittedO.length - 2]) == 404;
-	}).collect(Collectors.toList());
-    }
-
-    /**
-     * Чтобы filter не занимался чем не надо, чтение будест происходить отдельно
-     */
-    private static List<String> getListOfDataFromFile(String file) {
+    public static List<String> filter(String file, String pattern) {
 	try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-	    int inNext; // для проверки окончания файла
-	    StringBuilder dataStore = new StringBuilder();
-	    while ((inNext = in.read()) != -1) {
-		dataStore.append((char) inNext);
-	    }
-	    return Arrays.asList(dataStore.toString().split(System.lineSeparator()));
+	    return in.lines().filter(o -> Pattern.compile(pattern).matcher(o).find()).collect(Collectors.toList());
 	} catch (Exception e) {
-	    System.out.println(e.getStackTrace());
+	    e.printStackTrace();
+	    System.out.println("gg");
+	    return Collections.emptyList();
 	}
-	return Collections.emptyList();
     }
 
     /**
@@ -57,7 +44,8 @@ public class LogFilter {
     }
 
     public static void main(String[] args) {
-	List<String> log = filter("log.txt");
+	String pattern = "\\?*\\s404\\s\\d*";
+	List<String> log = filter("log.txt", pattern);
 	log.forEach(System.out::println);
 	saveToFile(log, "404.txt");
     }
