f9dad2f58a697b60e6477b93a6523a0eac3c813e
RinZ26 <ivs1996@mail.ru>
2020-06-02 11:56:59 +0500

    Проверить две строчки на идентичность. [#273008]

--- chapter_002_Input_Output/src/main/java/ru/job4j/collection/FreezeStr.java
+++ chapter_002_Input_Output/src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,20 +1,35 @@
 package ru.job4j.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Класс для проверок строк
  */
 public class FreezeStr {
+    /**
+     * Записываем в hashmap по ключу символа количество повторений в одной строке
+     * Во втором цикле в зависимости от совпадения снижаем количество повторений и по достижению ноля - ремуваем
+     * Если размер хэшмэпа == 0 => строки равны
+     */
     public static boolean eq(String left, String right) {
-	if (left.length() != right.length()) {
-	    return false;
-	}
-	StringBuilder builderForLeftString = new StringBuilder(left);
-	for (int c = 0; c < builderForLeftString.length(); c++) {
-	    if (builderForLeftString.indexOf(String.valueOf(right.charAt(c))) == -1) {
-		return false;
+	Map<Character, Integer> mapOfCountOfCharactersInLeftString = new HashMap<>();
+	for (Character character : left.toCharArray()) {
+	    if (mapOfCountOfCharactersInLeftString.containsKey(character)) {
+		mapOfCountOfCharactersInLeftString.put(character, mapOfCountOfCharactersInLeftString.get(character) + 1);
+	    } else {
+		mapOfCountOfCharactersInLeftString.put(character, 1);
 	    }
-	    builderForLeftString.deleteCharAt(c);
 	}
-	return true;
+	for (Character character : right.toCharArray()) {
+	    if (mapOfCountOfCharactersInLeftString.containsKey(character)) {
+		int size;
+		mapOfCountOfCharactersInLeftString.put(character, size = mapOfCountOfCharactersInLeftString.get(character) - 1);
+		if (size == 0) {
+		    mapOfCountOfCharactersInLeftString.remove(character);
+		}
+	    }
+	}
+	return mapOfCountOfCharactersInLeftString.size() == 0;
     }
 }
