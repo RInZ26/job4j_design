cf343cc7431134f533bea06e612156061211b5b3
RinZ26 <ivs1996@mail.ru>
2020-06-08 14:05:24 +0500

    6. Кодировка.[862#291404] Исправления

--- chapter_002_Input_Output/src/main/java/ru/job4j/io/SpeakWithMe.java
+++ chapter_002_Input_Output/src/main/java/ru/job4j/io/SpeakWithMe.java
@@ -3,9 +3,9 @@ package ru.job4j.io;
 import java.io.*;
 
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 
@@ -13,66 +13,46 @@ import java.util.stream.Collectors;
  * Класс для одиноких людей
  */
 public class SpeakWithMe {
-    private static final String FINISH_WORD = "Закончить";
-    private static final String STOP_WORD = "Стоп";
-    private static final String CONTINUE_WORD = "Продолжить";
-    Path pathSourceOfAnswers, pathLogOfConversation;
-    List<String> systemAnswersList = Collections.emptyList();
+    /**
+     * Попробуем вынести желание пользователя сюда в глобал
+     */
+    private static boolean userDesireOfSpeaking = true;
+    private static boolean isTheEnd;
 
-
-    public SpeakWithMe(Path pathSourceOfAnswers, Path pathLogOfConversation) {
-	this.pathSourceOfAnswers = pathSourceOfAnswers;
-	this.pathLogOfConversation = pathLogOfConversation;
+    public static void main(String[] args) {
+	wannaSpeak(Paths.get("C:\\projects\\SpeakWithMe\\systemAnswers.txt"), Paths.get("C:\\projects\\SpeakWithMe\\logOfConversation.txt"));
     }
 
-
     /**
-     * Консольный чат с записью ответов в лог.
-     * Данные берутся из внутренней коллекции, предварительно заполненной из файла(если такой есть).
-     * Поддерживаются команды - простые стринги. Енум здесь не особо нужен.
-     * Из минусов - учитывается регистр команд, плата за switch и неиспользование equalsIgnoreCase
-     * Команды: Стоп - программа перестаёт отвечать и НЕ ДАЁТ ОТВЕТ на команду стоп (важно)
-     * Продолжить - возобновляет общение - на саму команду ответа не даёт
-     * Закончить - выход из цикла
-     *
-     * По поводу использования коллекции и предварительной загрузки вместо работы с потоком BufferedReader на чтение файла ответов
-     * Надо уточнить у менторов: метод lines у bufferedreader ведёт себя очень странно и после одного обращения к стриму(lines) после терминальной операции
-     * оставляет итератор - где бог на душу положил. Поэтому повторный проход - не имеет смысла
-     * Есть возможность маркировки (до lines делаем mark, и потом через reset можем вернуться туда, где поставили закладку) - но это черная магия
+     * Запись в коллекцию из файл при частом обращении - оправдано! Потому что операция чтения дорогая.
+     * Держать стрим на запись долго - не надо, поэтому лог формируется в конце в отдельном минитрае
+     * Используется паттерн dispatcher в том или ином виде. Логика - избегать ифы и изменять код при расширении не здесь, а в диспатчере
+     * По поводу работы самого автоответчика:
+     * Есть два глобал булиана: isTheEnd - завершение работы, userDesireOfSpeaking - желание юзера получать ответы от системы
+     * Поддерживается три системные команды - они описаны в диспатчере.
      */
-    public void wannaSpeak() {
-	if (systemAnswersList.isEmpty()) {
-	    loadFile(pathSourceOfAnswers);
-	}
-	try (PrintWriter writerLog = new PrintWriter(new FileWriter(pathLogOfConversation.toString()), true);
-	     BufferedReader readerUser = new BufferedReader(new InputStreamReader(System.in))) {
-	    Random rnd = new Random();
-	    boolean userDesireOfSpeaking = true;
-	    String wordUser = "", wordSystem = "";
-	    while (!wordUser.equals(FINISH_WORD)) {
-		System.out.println("Say something human: ");
-		System.out.println("Команды: Закончить, Стоп, Продолжить");
-		wordUser = readerUser.readLine();
-		switch (wordUser) {
-		    case FINISH_WORD:
-		    case STOP_WORD:
-			userDesireOfSpeaking = false;
-			wordSystem = "";
-			break;
-		    case CONTINUE_WORD:
-			userDesireOfSpeaking = true;
-			wordSystem = "";
-			break;
-		    default:
-			wordSystem = userDesireOfSpeaking ? systemAnswersList.get(rnd.nextInt(systemAnswersList.size())) : "";
-			break;
-		}
-		if (userDesireOfSpeaking) {
-		    System.out.println(wordSystem + System.lineSeparator());
-		}
-		writerLog.printf("%s  -  %s" + System.lineSeparator(), wordUser, wordSystem);
+    public static void wannaSpeak(Path pathSourceOfAnswers, Path pathLogOfConversation) {
+	List<String> listOfSystemAnswers;
+	List<String> logOfConversation;
+	try (BufferedReader userReader = new BufferedReader(new InputStreamReader(System.in))) {
+	    DispatcherUserAnswers dispatcherUserAnswers = new DispatcherUserAnswers();
+	    listOfSystemAnswers = loadFile(pathSourceOfAnswers);
+	    logOfConversation = new ArrayList<>();
+	    String userQuestion, systemAnswer;
+	    while (!isTheEnd) {
+		System.out.println("Write something: ");
+		userQuestion = userReader.readLine();
+		System.out.println(systemAnswer = dispatcherUserAnswers.mapOfSystemInteractingWithUser.getOrDefault(userQuestion.toLowerCase(), dispatcherUserAnswers.defaultSystemAnswer(listOfSystemAnswers)).get());
+		logOfConversation.add(String.format("%s  -  %s", userQuestion, systemAnswer));
+	    }
+	    try (PrintWriter writerLog = new PrintWriter(new FileWriter(pathLogOfConversation.toString()))) {
+		logOfConversation.forEach(writerLog::println);
+	    } catch (Exception e) {
+		System.out.println("load log");
+		e.printStackTrace();
 	    }
 	} catch (Exception e) {
+	    System.out.println("reader init");
 	    e.printStackTrace();
 	}
     }
@@ -93,14 +73,86 @@ public class SpeakWithMe {
      * Заполнение коллекции ответов из файла с внутренней проверкой на возможность оного
      * Почему она вообще нужна - читай speakWithMe
      */
-    private boolean loadFile(Path pathAnswerFile) {
+    private static List<String> loadFile(Path pathAnswerFile) {
 	isExistAndFile(pathAnswerFile);
 	try (BufferedReader reader = new BufferedReader(new FileReader(pathAnswerFile.toString()))) {
-	    systemAnswersList = reader.lines().collect(Collectors.toList());
+	    return reader.lines().collect(Collectors.toList());
 	} catch (Exception e) {
 	    System.out.println("problem with load file");
 	    e.printStackTrace();
 	}
-	return true;
+	return Collections.emptyList();
     }
+
+    /**
+     * Используем хитрый диспатч вместо свичей https://github.com/peterarsentev/code_quality_principles
+     * У нас есть мапа по ключевым словам, за которыми закреплены определенные действия, выглядит неплохо, хотя и пришлось вынести булианы в global
+     */
+    private static class DispatcherUserAnswers {
+	Map<String, Supplier<String>> mapOfSystemInteractingWithUser = new HashMap<>();
+	private static final String FINISH_WORD = "закончить";
+	private static final String STOP_WORD = "стоп";
+	private static final String CONTINUE_WORD = "продолжить";
+	private static final String SYSTEM_SILENCED = "";
+
+	DispatcherUserAnswers() {
+	    init();
+	}
+
+	// Init all system words and it functions
+	void init() {
+	    mapOfSystemInteractingWithUser.put(FINISH_WORD, finishWordSupplier());
+	    mapOfSystemInteractingWithUser.put(CONTINUE_WORD, continueWordSupplier());
+	    mapOfSystemInteractingWithUser.put(STOP_WORD, stopWordSupplier());
+	}
+
+	/**
+	 * Этот метод и далее просто заносят логику действий, которые должны выполняться при вводе команды пользователем
+	 */
+	private Supplier<String> finishWordSupplier() {
+	    return () -> {
+		userDesireOfSpeaking = false;
+		isTheEnd = true;
+		return SYSTEM_SILENCED;
+	    };
+	}
+
+	private Supplier<String> continueWordSupplier() {
+	    return () -> {
+		userDesireOfSpeaking = true;
+		return SYSTEM_SILENCED;
+	    };
+	}
+
+	private Supplier<String> stopWordSupplier() {
+	    return () -> {
+		userDesireOfSpeaking = false;
+		return SYSTEM_SILENCED;
+	    };
+	}
+
+	/**
+	 * Эта штука выполняется, когда ничего не нашлось в мапе посредством getOrDefault
+	 *
+	 * @param listOfSystemAnswers ссылка на коллекцию, с которой работаем
+	 */
+	private Supplier<String> defaultSystemAnswer(List<String> listOfSystemAnswers) {
+	    Random rnd = new Random();
+	    return () -> userDesireOfSpeaking ? listOfSystemAnswers.get(rnd.nextInt(listOfSystemAnswers.size())) : SYSTEM_SILENCED;
+	}
+
+    }
+    //Попытка дружить с enum вместо стрингов
+/*
+    enum SystemAnswers {
+	FINISH_WORD("Закончить"),
+	STOP_WORD("Стоп"),
+	CONTINUE_WORD("Продолжить");
+	String value;
+
+	SystemAnswers(String value) {
+	    this.value = value;
+	}
+    }
+    */
 }
