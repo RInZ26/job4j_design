c6c5307aca47642d1d216219eb1c36fda89c9d8f
RinZ26 <ivs1996@mail.ru>
2020-06-08 11:57:44 +0500

    Поиски правды у BufferedReader.lines()

---
+++ chapter_002_Input_Output/src/main/java/ru/job4j/io/ReadBufferLines.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReadBufferLines {
+    public static void main(String[] args) {
+	Path pathSource = Paths.get("C:\\test\\source.txt");
+	try (BufferedReader reader = new BufferedReader(new FileReader(pathSource.toString()));
+	     PrintWriter writer = new PrintWriter(new FileWriter(pathSource.toString()), true)) {
+	    List<String> sourceList = Arrays.asList("МетодLines ЖуткоКОВАРЕН".split(" "));
+	    sourceList.forEach(line -> writer.print(line + System.lineSeparator())); // запись в файл
+	    writer.close();
+
+	    System.out.println(reader.lines().findFirst().get()); //МетодLines
+	    System.out.println(reader.lines().findFirst().get()); // ?? ЖуткоКоварен
+	} catch (Exception e) {
+	    System.out.println("reader / writer init");
+	    e.printStackTrace();
+	}
+    }
+}
