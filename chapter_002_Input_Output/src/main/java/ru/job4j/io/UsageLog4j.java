0602d4a46ae51ee297e6428410f2f2894dbb1559
RinZ26 <ivs1996@mail.ru>
2020-06-11 13:08:09 +0500

    3. Slf4j - вывод переменных.[268852#291391]

--- chapter_002_Input_Output/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002_Input_Output/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -5,13 +5,23 @@ import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(
+            UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte paramByte = 1;
+        short paramShort = 2;
+        char paramChar = 3;
+        int paramInt = 4;
+        long paramLong = 5;
+        float paramFloat = 6;
+        double paramDouble = 7;
+        boolean paramBoolean = false;
+        LOG.debug("paramByte : {}, paramShort : {}, paramChar : {},", paramByte,
+                  paramShort, paramChar);
+        LOG.debug("paramInt : {}, paramLong : {}, paramFloat : {},", paramInt,
+                  paramLong, paramFloat);
+        LOG.debug("paramDouble : {}, paramBoolean : {} ", paramDouble,
+                  paramBoolean);
     }
 }
