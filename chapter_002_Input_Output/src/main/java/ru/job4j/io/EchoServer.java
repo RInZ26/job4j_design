a73a991ec4b73a236a13f2e8c02759d3965af299
RinZ26 <ivs1996@mail.ru>
2020-06-11 12:04:15 +0500

    1. Бот[7921#291412] Исправления

--- chapter_002_Input_Output/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002_Input_Output/src/main/java/ru/job4j/io/EchoServer.java
@@ -20,108 +20,104 @@ public class EchoServer {
      * использовать регулярки - плохо, то здесь всё также делается примитивно
      * через работу со стрингом Важно! На забывать отправить OK_Request клиенту,
      * потому что иначе страница будет висеть.
+     * message - сообщение от пользователя
      */
     public static void main(String[] args) throws IOException {
-	try (ServerSocket server = new ServerSocket(9000)) {
-	    DispatcherTextRequest dispatcherTextRequest =
-		    new DispatcherTextRequest(server);
-	    while (!server.isClosed()) {
-		Socket socket = server.accept();
-		try (OutputStream out = socket.getOutputStream();
-		     BufferedReader in = new BufferedReader(
-			     new InputStreamReader(socket.getInputStream()))) {
-		    String str, messageFromRequest = null;
-		    while (!(str = in.readLine()).isEmpty()) {
-			if (messageFromRequest
-				== null) { //first line from current session
-			    messageFromRequest = getMessageFromRequest(str);
-			}
-			System.out.println(str);
-		    }
-		    if (messageFromRequest != null && !messageFromRequest
-			    .equals("")) {
-			out.write(OK_REQUEST.getBytes());
-			out.write(dispatcherTextRequest.mapOfSystemUserRequests
-					  .getOrDefault(messageFromRequest
-								.toLowerCase(),
-							dispatcherTextRequest
-								.defaultSupplier(
-									messageFromRequest))
-					  .get().getBytes());
-		    }
-		}
-	    }
-	}
+        try (ServerSocket server = new ServerSocket(9000)) {
+            Dispatcher dispatcher = new Dispatcher(server);
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String line, message = null;
+                    while (!(line = in.readLine()).isEmpty()) {
+                        if (message == null) { //first line from current session
+                            message = getMessage(line);
+                        }
+                        System.out.println(line);
+                    }
+                    if (message != null && !message.equals("")) {
+                        out.write(OK_REQUEST.getBytes());
+                        out.write(dispatcher.systemWords.getOrDefault(
+                                message.toLowerCase(),
+                                dispatcher.defaultSupplier(message))
+                                .get()
+                                .getBytes());
+                    }
+                }
+            }
+        }
     }
 
     /**
      * Попытка поймать msg, которым мы получем
      *
      * @param request
-     *
      * @return
      */
-    private static String getMessageFromRequest(String request) {
-	String preMessageTemplate = "/?msg=";
-	int indexOfMsgStart = request.indexOf("/?msg=");
-	return indexOfMsgStart != -1 ? request
-		.substring(indexOfMsgStart + preMessageTemplate.length())
-		.split(" ")[0] : "";
+    private static String getMessage(String request) {
+        String preMessageTemplate = "/?msg=";
+        int indexOfMsgStart = request.indexOf("/?msg=");
+        return indexOfMsgStart != -1
+                ? request.substring(
+                indexOfMsgStart + preMessageTemplate.length()).split(" ")[0]
+                : "";
     }
 
     /**
      * Диспачтер, смотри SpeakWithMe
      */
-    private static class DispatcherTextRequest {
-	private static final String MESSAGE_HELLO = "hello";
-	private static final String MESSAGE_EXIT = "exit";
-	private static final String MESSAGE_WHAT = "what";
-	ServerSocket server;
-	Map<String, Supplier<String>> mapOfSystemUserRequests = new HashMap<>();
+    private static class Dispatcher {
+        private static final String MESSAGE_HELLO = "hello";
+        private static final String MESSAGE_EXIT = "exit";
+        private static final String MESSAGE_WHAT = "what";
+        ServerSocket server;
+        Map<String, Supplier<String>> systemWords = new HashMap<>();
 
-	/**
-	 * Чтобы не выносить в глобал переменные, которые выносить вовсе не
-	 * нужно, проще их тут закинуть В данном случае мы работамем с сервером,
-	 * поэтому нам нужна ссылка на него
-	 *
-	 * @param serverSocket
-	 */
-	DispatcherTextRequest(ServerSocket serverSocket) {
-	    this.server = serverSocket;
-	    initDispatcher();
-	}
+        /**
+         * Чтобы не выносить в глобал переменные, которые выносить вовсе не
+         * нужно, проще их тут закинуть В данном случае мы работамем с сервером,
+         * поэтому нам нужна ссылка на него
+         *
+         * @param serverSocket
+         */
+        Dispatcher(ServerSocket serverSocket) {
+            this.server = serverSocket;
+            initDispatcher();
+        }
 
-	/**
-	 * заполенение мапы нашими связками
-	 */
-	public void initDispatcher() {
-	    mapOfSystemUserRequests.put(MESSAGE_HELLO, msgHelloSupplier());
-	    mapOfSystemUserRequests.put(MESSAGE_WHAT, msgWhatSupplier());
-	    mapOfSystemUserRequests.put(MESSAGE_EXIT, msgExitSupplier());
-	}
+        /**
+         * заполенение мапы нашими связками
+         */
+        public void initDispatcher() {
+            systemWords.put(MESSAGE_HELLO, msgHelloSupplier());
+            systemWords.put(MESSAGE_WHAT, msgWhatSupplier());
+            systemWords.put(MESSAGE_EXIT, msgExitSupplier());
+        }
 
-	private Supplier<String> msgHelloSupplier() {
-	    return (() -> "Hello there");
-	}
+        private Supplier<String> msgHelloSupplier() {
+            return (() -> "Hello there");
+        }
 
-	private Supplier<String> msgExitSupplier() {
-	    return (() -> {
-		try {
-		    server.close();
-		} catch (IOException io) {
-		    System.out
-			    .println("close Server by dispatcher exitSupplier");
-		}
-		return "";
-	    });
-	}
+        private Supplier<String> msgExitSupplier() {
+            return (() -> {
+                try {
+                    server.close();
+                } catch (IOException io) {
+                    System.out.println(
+                            "close Server by dispatcher exitSupplier");
+                }
+                return "";
+            });
+        }
 
-	private Supplier<String> msgWhatSupplier() {
-	    return (() -> "What what?");
-	}
+        private Supplier<String> msgWhatSupplier() {
+            return (() -> "What what?");
+        }
 
-	private Supplier<String> defaultSupplier(String request) {
-	    return () -> request;
-	}
+        private Supplier<String> defaultSupplier(String request) {
+            return () -> request;
+        }
     }
 }
