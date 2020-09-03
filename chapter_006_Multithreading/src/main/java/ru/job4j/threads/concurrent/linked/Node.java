b0d85fd63091e1f5092f8bdc83bfe1f8e2eaa601
RinZ26 <ivs1996@mail.ru>
2020-09-03 11:09:47 +0500

    3. Immutable объекты[267918#349578]  Причёсывание аннотаций

--- chapter_006_Multithreading/src/main/java/ru/job4j/threads/concurrent/linked/Node.java
+++ chapter_006_Multithreading/src/main/java/ru/job4j/threads/concurrent/linked/Node.java
@@ -1,14 +1,12 @@
 package ru.job4j.threads.concurrent.linked;
 
 import javax.annotation.concurrent.Immutable;
-import javax.annotation.concurrent.NotThreadSafe;
 
 /**
  * Если хотим поддерживать иммутабельность объектов с изменяющимся полями,
  * нужно вместо сеттеров использовать пересоздание объектов, а все поля
  * сделать final
  */
-@NotThreadSafe
 @Immutable //сама аннотация ничего не делает, она просто маркер
 public class Node<T> {
     private final Node<T> next;
