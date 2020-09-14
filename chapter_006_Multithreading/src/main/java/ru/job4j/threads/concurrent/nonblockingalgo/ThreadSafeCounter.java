f7052219032e6d30b0a74569bcf3cf3d2ebb63c6
RinZ26 <ivs1996@mail.ru>
2020-09-14 10:41:48 +0500

    0. CAS - операции[6859#349592] v2

--- chapter_006_Multithreading/src/main/java/ru/job4j/threads/concurrent/nonblockingalgo/ThreadSafeCounter.java
+++ chapter_006_Multithreading/src/main/java/ru/job4j/threads/concurrent/nonblockingalgo/ThreadSafeCounter.java
@@ -2,20 +2,32 @@ package ru.job4j.threads.concurrent.nonblockingalgo;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Потокобезопасный за счёт атомиков счётчик
  */
 @ThreadSafe
-public class ThreadSafeCounter<T> {
-    private final LongAdder count = new LongAdder();
+public class ThreadSafeCounter {
+    /**
+     * Т.к. поддерживает CAS операции
+     */
+    private final AtomicReference<Integer> atomicInc = new AtomicReference<>(0);
 
+    /**
+     * Трактуем эту операцию как CAS и увеличиваем только в том случае, если
+     * значение ещё никто не поменял из других потоков
+     */
     public void increment() {
-        count.increment();
+        int expected = atomicInc.get();
+        atomicInc.compareAndSet(expected, expected + 1);
     }
 
+    /**
+     * За счёт volatile value внутри AtomicReference должно вернуться
+     * актуальное значение на данный момент
+     */
     public int get() {
-        return count.intValue();
+        return atomicInc.get();
     }
 }
